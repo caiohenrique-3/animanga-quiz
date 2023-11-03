a15f275137affd257497782fcc8f95b2da582193
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-03 11:04:58 -0300

    feat: Add quiz start button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+// dependencies
+import { useState } from "react";
+
 // css
 import "./styles/index.css";
 import "./styles/fonts.css";
 
 function App() {
+  const [isQuizStarted, setIsQuizStarted] = useState(false);
+
+  function handleQuizStartButton() {
+    setIsQuizStarted(true);
+  }
+
   return (
     <div className="container">
-      <h1>Hello World!</h1>
+      {!isQuizStarted && (
+        <button id="start-quiz-button" onClick={handleQuizStartButton}>
+          Start quiz
+        </button>
+      )}
+
+      {isQuizStarted && (
+        <p>
+          Lorem ipsum dolor sit amet
+        </p>
+      )}
     </div>
   );
 }
