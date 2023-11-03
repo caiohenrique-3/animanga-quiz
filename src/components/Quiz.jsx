a93b006d09873a7518b5838522ddae818fc84363
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-03 17:39:32 -0300

    feat: Implement core quiz functionalities

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,29 +7,110 @@ import "../styles/quiz.css";
 
 export default function Quiz() {
   const [questions, setQuestions] = useState([]);
+  const [checkResults, setCheckResults] = useState(false);
 
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=10&category=31&type=multiple")
-      .then((res) => res.json())
-      .then((data) => setQuestions(data.results));
+    fetchQuestions();
   }, []);
 
-  const questionElements = questions.map((question, index) => (
-    <div key={index} className="question">
+  const fetchQuestions = () => {
+    fetch("https://opentdb.com/api.php?amount=10&category=31&type=multiple")
+      .then((res) => res.json())
+      .then((data) => {
+        // Shuffle the answers for each question
+        const shuffledQuestions = data.results.map((question) => ({
+          ...question,
+          answers: shuffleArray([
+            ...question.incorrect_answers,
+            question.correct_answer,
+          ]),
+          selectedAnswer: null,
+        }));
+        setQuestions(shuffledQuestions);
+      });
+  };
+
+  // Function to shuffle an array
+  const shuffleArray = (array) => {
+    let newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
+
+  const handleAnswerSelect = (questionIndex, answerIndex) => {
+    setQuestions((prevQuestions) => {
+      const updatedQuestions = [...prevQuestions];
+      updatedQuestions[questionIndex].selectedAnswer = answerIndex;
+      return updatedQuestions;
+    });
+  };
+
+  const questionElements = questions.map((question, questionIndex) => (
+    <div key={questionIndex} className="question">
       <h3>{he.decode(question.question)}</h3>
       <ul>
-        {question.incorrect_answers.map((answer, i) => (
-          <li key={i}>{he.decode(answer)}</li>
-        ))}
-        <li>{he.decode(question.correct_answer)}</li>
+        {question.answers.map((answer, answerIndex) => {
+          let answerClass = "";
+          if (checkResults) {
+            if (answer === question.correct_answer) {
+              answerClass = "correct";
+            } else if (question.selectedAnswer === answerIndex) {
+              answerClass = "incorrect";
+            }
+          } else if (question.selectedAnswer === answerIndex) {
+            answerClass = "selected";
+          }
+
+          return (
+            <li
+              key={answerIndex}
+              onClick={() => handleAnswerSelect(questionIndex, answerIndex)}
+              className={answerClass}
+            >
+              {he.decode(answer)}
+            </li>
+          );
+        })}
       </ul>
     </div>
   ));
+
+  const handleCheckResults = () => {
+    if (checkResults) {
+      // Reset the quiz
+      fetchQuestions();
+      setCheckResults(false);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      const unansweredQuestions = questions.filter(
+        (question) => question.selectedAnswer === null,
+      );
+
+      if (unansweredQuestions.length > 0) {
+        alert("Please answer all questions before checking results.");
+      } else {
+        setCheckResults(true);
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
 
   return (
     <main>
       <div className="questions-container">
         {questionElements}
+        <button onClick={handleCheckResults} id="check-restart-button">
+          {checkResults ? "Reset game" : "Check results"}
+        </button>
       </div>
     </main>
   );
